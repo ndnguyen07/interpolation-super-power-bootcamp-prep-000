547a414f3cbf37e73e6f4a07ef7f187f748a2756
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-04 01:36:55 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@
 colors = (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 
 def display_rainbow(colors)
-  puts "R: #{red}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
+  puts "R: #{colors[1]}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
 end
 
 display_rainbow(colors)
